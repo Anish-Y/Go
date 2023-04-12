084215e9e589ce8bd0b9dd1fa228aa2a0b37dd02
s112415 <insertionsortaops@gmail.com>
2023-04-12 13:36:43 -0500

    Add onClick listener to each past game button

--- app/src/main/java/com/team2/gogame/PastGamesActivity.java
+++ app/src/main/java/com/team2/gogame/PastGamesActivity.java
@@ -1,6 +1,8 @@
 package com.team2.gogame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -29,6 +31,11 @@ public class PastGamesActivity extends AppCompatActivity {
             Button b = new Button(this);
             b.setId(game.getId());
             b.setText(game.toString());
+            b.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    startActivity(new Intent(PastGamesActivity.this, PastGamesActivity.class));
+                }
+            });
             group.addView(b);
         }
         s.addView(group);
